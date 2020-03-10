b8c15bb484964572c912e63203b34d195190b29c
cgyuan <2515380940@qq.com>
2020-03-10 10:28:52 +0800

    fix: use Activity context to obtain InputMethodManager cause Activity leak; Solution: replace context with application context

--- app/src/main/java/com/cyuan/bimibimi/core/extension/Activity.kt
+++ app/src/main/java/com/cyuan/bimibimi/core/extension/Activity.kt
@@ -12,7 +12,7 @@ fun Activity.hideSoftKeyboard() {
         val view = currentFocus
         if (view != null) {
             val binder = view.windowToken
-            val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val manager = application.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             manager.hideSoftInputFromWindow(binder, InputMethodManager.HIDE_NOT_ALWAYS)
         }
     } catch (e: Exception) {
